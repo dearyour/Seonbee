cb5db211ac9d7dcc036b3cd59c623beb976c049e
kjin <kjin4_1@naver.com>
2022-04-20 16:51:03 +0900

    #S06P31A406-131 feat: backend skeleton code spring boot version 2.5.0

---
+++ backend/src/main/java/com/seonbi/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.seonbi.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Autowired
+//    private MemberDetailService
+
+}
