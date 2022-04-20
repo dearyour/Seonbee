c6102f0405980436ec9a98aa42ddd1d6aee2cbc9
kjin <kjin4_1@naver.com>
2022-04-20 23:58:52 +0900

    #S06P31A406-134 feat: member entity

--- backend/src/main/java/com/seonbi/db/entity/BaseEntity.java
+++ backend/src/main/java/com/seonbi/db/entity/BaseEntity.java
@@ -1,18 +1,30 @@
 package com.seonbi.db.entity;
 
 import lombok.Getter;
-import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 /*
     모델 간 공통 사항 정의
  */
 @Getter
 @MappedSuperclass   // 상속을 위해
-@EntityListeners(AuditingEntityListener.class)  // https://sas-study.tistory.com/361 참고
+@EntityListeners(AuditingEntityListener.class)  // @CreatedDate, @LastModifiedDate 어노테이션을 이용
 public class BaseEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    LocalDateTime createdDate;  // 생성 날짜
+
+    @LastModifiedBy
+    LocalDateTime modifiedDate; // 수정 날짜
+
+    Boolean isDeleted=false;    // 지워졌는지 여부
+
 
 }
