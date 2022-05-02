055c242ae44139f72445848fe186f3fe11fca923
Elgcha <vkdlfl681@gmail.com>
2022-05-02 16:24:29 +0900

    #S06P31A406-269 feat: navbar edit

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,18 @@
-import '../styles/globals.css'
-import 'react-calendar/dist/Calendar.css';
+import "../styles/globals.css";
+import "react-calendar/dist/Calendar.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../styles/join/sign.css";
 import type { AppProps } from "next/app";
 import wrapper from "store/index";
-import Navbar from 'components/commons/bars/Navbar';
-
+import Navbar from "components/commons/bars/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <div><Navbar></Navbar><Component {...pageProps} /></div>;
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
-import Navbar from 'components/commons/bars/Navbar';
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import React from "react";
+import Navbar from "components/commons/bars/Navbar";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Btn from "components/commons/Btn";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="">
       <Head>
         <title>당신의 선물비서, 선비</title>
       </Head>
-      <div>
-        {/* <Navbar /> */}
+      <div className="container">
+        <div className="row">
+          <div className="col-8">
+            <h3>
+              당신의 선물비서, <span>선비</span>가
+            </h3>
+            <h3>선물을 추천 해드립니다.</h3>
+            <h5>당신의 소중한 사람만을 위한 맞춤형 선물을 추천 받으세요!</h5>
+            <div>
+              <Btn>지금 추천받기</Btn>
+              <Btn>선비에 가입하기</Btn>
+            </div>
+          </div>
+          <div className="col"></div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
