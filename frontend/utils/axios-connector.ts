091f57b223dd9ce32a9faad1ab90548edbe88af4
Elgcha <vkdlfl681@gmail.com>
2022-05-04 13:46:28 +0900

    #S06P31A406-285 feat: social page search

--- frontend/utils/axios-connector.ts
+++ frontend/utils/axios-connector.ts
@@ -7,10 +7,10 @@ const axiosConnector = axios.create({
 
 axiosConnector.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("jwt");
+    const token = sessionStorage.getItem("Token");
 
     if (token && config.headers) {
-      config.headers["Authorization"] = token;
+      config.headers["Authorization"] = "Bearer " + token;
     } else {
       config.withCredentials = false;
     }
