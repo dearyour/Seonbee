be3c0d3551633aa19de3be9c1a08c257610dfeb5
kjin <kjin4_1@naver.com>
2022-05-08 16:16:39 +0900

    fix: delete schedule error status

--- backend/src/main/java/com/seonbi/api/controller/LanternController.java
+++ backend/src/main/java/com/seonbi/api/controller/LanternController.java
@@ -125,7 +125,7 @@ public class LanternController {
         }
         int deleteLanternCode=lanternService.deleteLantern(member.getMemberId(), lanternId);
         if (deleteLanternCode==401)     return ResponseEntity.status(401).body(BaseResponseBody.of(401, "유효하지 않는 연등입니다."));
-        else if (deleteLanternCode==403)     return ResponseEntity.status(403).body(BaseResponseBody.of(401, "사용자 권한이 없습니다."));
+        else if (deleteLanternCode==403)     return ResponseEntity.status(403).body(BaseResponseBody.of(403, "사용자 권한이 없습니다."));
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "success"));
     }
 
@@ -139,7 +139,7 @@ public class LanternController {
         }
         int deleteScheduleCode=lanternService.deleteSchedule(member.getMemberId(), scheduleId);
         if (deleteScheduleCode==401)     return ResponseEntity.status(401).body(BaseResponseBody.of(401, "유효하지 않는 연등입니다."));
-        else if (deleteScheduleCode==403)     return ResponseEntity.status(403).body(BaseResponseBody.of(401, "사용자 권한이 없습니다."));
+        else if (deleteScheduleCode==403)     return ResponseEntity.status(403).body(BaseResponseBody.of(403, "사용자 권한이 없습니다."));
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "success"));
     }
 
