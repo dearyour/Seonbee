65eee41214b48f140684fcfbdf30c5a648025348
Elgcha <vkdlfl681@gmail.com>
2022-05-10 18:24:27 +0900

    fix: build error fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,14 @@
+var CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["picsum.photos"],
   },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+    // other plugins ...
+  ],
   env: {
     NEXT_PUBLIC_BACK: process.env.NEXT_PUBLIC_BACK,
     KAKAO_LOGIN: process.env.KAKAO_LOGIN,
