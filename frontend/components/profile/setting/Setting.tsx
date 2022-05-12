31d81cf373eb4bbc742529619ba84d4f75ef1b9e
Elgcha <vkdlfl681@gmail.com>
2022-05-12 16:41:04 +0900

    #S06P31A406-358 feat: mypage 주고싶소 기능

--- frontend/components/profile/setting/Setting.tsx
+++ frontend/components/profile/setting/Setting.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useProfile from "store/hook/profileHooks";
+import axiosConnector from "utils/axios-connector";
 
 type Props = {};
 
+interface Data {
+  memberId: number;
+  birthday: string;
+  email: string;
+  nickname: string;
+  gender: string;
+  mbti: string;
+  interest: string;
+  likelist: string;
+  banlist: string;
+  isAdmin: boolean;
+  isKakao: boolean;
+  image: File;
+  verse: string;
+}
+
 const Setting = (props: Props) => {
+  const [mydata, setMydata] = useState<Data>();
+  const { hostId } = useProfile();
+  useEffect(() => {
+    axiosConnector({
+      method: "GET",
+      url: "profile/" + String(hostId),
+    })
+      .then((res) => {
+        console.log(res);
+        setMydata(res.data.member);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
   return (
-    <div className="row">
-      <div className="col-4">test</div>
-      <div className="col-8">test2</div>
+    <div className="row w-100">
+      <div className="col-12 col-md-4">test</div>
+      <div className="col-12 col-md-8">test2</div>
     </div>
   );
 };
