a0cb08370f238b625ff7d31ce4b5aae927a17f97
jaehyun <keejj9292@naver.com>
2022-05-13 14:44:36 +0900

    #S06P31A406-349 feat: 상품 추천 api

---
+++ backend/src/main/java/com/seonbi/api/controller/RecommendController.java
@@ -0,0 +1,32 @@
+package com.seonbi.api.controller;
+
+
+import com.seonbi.api.request.ReceiverInfoReq;
+import com.seonbi.api.response.BaseResponseBody;
+import com.seonbi.api.service.RecommendService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/recommend")
+public class RecommendController {
+
+    @Autowired
+    RecommendService recommendService;
+
+
+    @PostMapping("/receiver")
+    public ResponseEntity<? extends BaseResponseBody> recommend(@RequestBody ReceiverInfoReq receiverInfoReq)
+
+    {
+
+        recommendService.ProductRecommend(receiverInfoReq);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+    }
+
+
+}
--- backend/src/main/java/com/seonbi/api/service/WordServiceImpl.java
+++ backend/src/main/java/com/seonbi/api/service/WordServiceImpl.java
@@ -14,18 +14,18 @@ import java.util.Optional;
 @Service
 public class WordServiceImpl implements WordService {
 
-//    static String list[] ={"감사","개업","결혼","결혼축하","기념일","돌선물","백일","생일","서프라이즈"
-//    ,"위로","응원","임신","집들이","창업","출산","enfj","enfp","entj","entp","esfj","esfp","estj","estp",
-//            "infj","infp","intj","intp","isfj","isfp","istj","istp","가족","남자친구","남자형제","남편",
-//            "멘토","배우자","부모님","사촌","스승","아내","아버지","어머니","여자친구","여자형제","연인","지인",
-//            "친구","친인척","친한친구","회사동기","회사동료","회사선배","회사후배","sns","가족","건강","게임","공예",
-//            "교육","동물","뷰티","쇼핑","엔터테인먼트","여행","연예인","요리","운동","음식","음악","음주","인테리어",
-//            "전자제품","캠핑","패션","10대","20대","30대","남자","여자"
-//    };
+    //인스타로만 하면 여자형제는 아무것도 안뜸 "여자형제",
 
-
-    static String list[] ={"남자형제"
+    static String list[] ={"감사","개업","결혼","결혼축하","기념일","돌선물","백일","생일","서프라이즈"
+    ,"위로","응원","임신","집들이","창업","출산","enfj","enfp","entj","entp","esfj","esfp","estj","estp",
+            "infj","infp","intj","intp","isfj","isfp","istj","istp","가족","남자친구","남자형제","남편",
+            "멘토","배우자","부모님","사촌","스승","아내","아버지","어머니","여자친구","연인","지인",
+            "친구","친인척","친한친구","회사동기","회사동료","회사선배","회사후배","sns","가족","건강","게임","공예",
+            "교육","동물","뷰티","쇼핑","엔터테인먼트","여행","연예인","요리","운동","음식","음악","음주","인테리어",
+            "전자제품","캠핑","패션","10대","20대","30대","40대","50대","60대","남자","여자"
     };
+
+
 
 
     @Autowired
@@ -38,7 +38,7 @@ public class WordServiceImpl implements WordService {
         for(int k=0; k< list.length; k++)
         {
             String sub = list[k];
-//            System.out.println("현재 sub="+sub);
+             System.out.println("현재 sub="+sub);
             List<List<String>> result= WordServiceImpl.readCSV(sub);
 
             for(int i=0; i<result.size(); i++)
@@ -96,7 +96,7 @@ public class WordServiceImpl implements WordService {
             br = new BufferedReader(new FileReader(csv));
             while ((line = br.readLine()) != null) { // readLine()은 파일에서 개행된 한 줄의 데이터를 읽어온다.
                 List<String> aLine = new ArrayList<String>();
-                String[] lineArr = line.split(","); // 파일의 한 줄을 ,로 나누어 배열에 저장 후 리스트로 변환한다.
+                String[] lineArr = line.split(" "); // 파일의 한 줄을 ,로 나누어 배열에 저장 후 리스트로 변환한다.
                 aLine = Arrays.asList(lineArr);
                 aLine.set(0,subject);  //맨첫번째는 주제로
                 csvList.add(aLine);
