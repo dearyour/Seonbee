926d94da3c96be0b95e56a980380decdbe669bba
Elgcha <vkdlfl681@gmail.com>
2022-05-19 10:48:59 +0900

    #S06P31A406-408 feat: 저잣거리 맨 위로 버튼, 디자인 수정

--- frontend/pages/recommend.tsx
+++ frontend/pages/recommend.tsx
@@ -1,49 +1,49 @@
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
-import { useEffectOnce } from 'store/hook/useEffectOnce';
-import Image from 'next/image';
-import Hobee from 'public/seonbee.png';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffectOnce } from "store/hook/useEffectOnce";
+import Image from "next/image";
+import Hobee from "public/seonbee.png";
 import {
   LeftSpeechBubble,
   RecommendContainer,
-} from 'styles/chat/RecommendElements';
+} from "styles/chat/RecommendElements";
 import {
   ProductsContent,
   Card,
   CardImg,
   CardContent,
   Price,
-} from 'styles/chat/ProductsElements';
-import { Button, Stack } from '@mui/material';
-import Btn from 'components/commons/Btn';
-import { AiOutlineLogin, AiOutlineSave } from 'react-icons/ai';
-import EllipsisText from 'react-ellipsis-text';
-import axiosConnector from 'utils/axios-connector';
+} from "styles/chat/ProductsElements";
+import { Button, Stack } from "@mui/material";
+import Btn from "components/commons/Btn";
+import { AiOutlineLogin, AiOutlineSave } from "react-icons/ai";
+import EllipsisText from "react-ellipsis-text";
+import axiosConnector from "utils/axios-connector";
 
 const dummyProductList = [
   {
     recommendId: 10,
     productId: 1,
-    name: '상품 이름 1 그런데 상품 이름이 너무 길어요!! 너무너무너무너무 너무너무너무 길어요',
+    name: "상품 이름 1 그런데 상품 이름이 너무 길어요!! 너무너무너무너무 너무너무너무 길어요",
     price: 10000,
-    buyUrl: 'https://www.naver.com',
-    imageUrl: 'https://picsum.photos/200/300',
+    buyUrl: "https://www.naver.com",
+    imageUrl: "https://picsum.photos/200/300",
   },
   {
     recommendId: 20,
     productId: 2,
-    name: '상품 이름 2 상품 이름이 조금 길어요.. 조금',
+    name: "상품 이름 2 상품 이름이 조금 길어요.. 조금",
     price: 20000,
-    buyUrl: 'https://www.naver.com',
-    imageUrl: 'https://picsum.photos/200/300',
+    buyUrl: "https://www.naver.com",
+    imageUrl: "https://picsum.photos/200/300",
   },
   {
     recommendId: 30,
     productId: 3,
-    name: '상품 이름이 짧아요',
+    name: "상품 이름이 짧아요",
     price: 30000,
-    buyUrl: 'https://www.naver.com',
-    imageUrl: 'https://picsum.photos/200/300',
+    buyUrl: "https://www.naver.com",
+    imageUrl: "https://picsum.photos/200/300",
   },
 ];
 
@@ -60,6 +60,8 @@ function Recommend() {
   const router = useRouter();
 
   const [productList, setProductList] = useState<ProductType[]>([]);
+  const [isMember, setIsMember] = useState<boolean>(false);
+
   const [isSaved, setIsSaved] = useState<{ rid: number; state: boolean }[]>([
     {
       rid: dummyProductList[0].recommendId,
@@ -77,21 +79,21 @@ function Recommend() {
 
   useEffectOnce(() => {
     if (Object.keys(router.query).length === 0) {
-      router.push('/404');
+      router.push("/404");
       return;
     }
 
     console.log(router.query);
-    console.log('age', typeof router.query.age);
-    console.log('mbti', typeof router.query.mbti);
-    console.log('price', typeof router.query.price);
+    console.log("age", typeof router.query.age);
+    console.log("mbti", typeof router.query.mbti);
+    console.log("price", typeof router.query.price);
 
     const { age, name, price, gender, mbti, interest, relation, purpose } =
       router.query;
 
     axiosConnector({
-      method: 'POST',
-      url: 'recommend/receiver',
+      method: "POST",
+      url: "recommend/receiver",
       data: {
         age: Number(age),
         name: name,
@@ -112,7 +114,7 @@ function Recommend() {
   });
 
   const saveAll = () => {
-    console.log('saved all');
+    console.log("saved all");
 
     for (let i = 0; i < productList.length; i++) {
       const rid = productList[i].recommendId;
@@ -136,7 +138,7 @@ function Recommend() {
         v.rid === rid ? { ...v, state: !v.state } : v
       )
     );
-    console.log(rid, 'saved');
+    console.log(rid, "saved");
 
     // TODO: 주고싶소 저장 api
   };
@@ -152,29 +154,29 @@ function Recommend() {
             alt="item-imageUrl"
             width={150}
             height={150}
-            style={{ borderRadius: '5px' }}
+            style={{ borderRadius: "5px" }}
           />
         </CardImg>
         <CardContent>
           <h2>
-            <EllipsisText text={item.name} length={'30'} />
+            <EllipsisText text={item.name} length={"30"} />
           </h2>
           {/* <p>{item.name}</p> */}
           <Price>
-            {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')} 원
+            {item.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} 원
           </Price>
           <Stack direction="row" spacing={2}>
             <a href={item.buyUrl} target="_blank" rel="noopener noreferrer">
               <Btn>상품 구경하기</Btn>
             </a>
-            {sessionStorage.getItem('Token') &&
+            {sessionStorage.getItem("Token") &&
               (isSaved[i].state ? (
                 <Button
                   disabled
                   style={{
-                    fontSize: '1rem',
-                    fontWeight: 'bold',
-                    color: 'rgba(0,0,0,0.4)',
+                    fontSize: "1rem",
+                    fontWeight: "bold",
+                    color: "rgba(0,0,0,0.4)",
                   }}
                 >
                   저장되었소
@@ -243,7 +245,7 @@ function Recommend() {
           {router.query.name}님이 기뻐할 것이오.
         </LeftSpeechBubble>
       </Stack>
-      {sessionStorage.getItem('Token') ? (
+      {sessionStorage.getItem("Token") ? (
         <Btn filled={true}>
           <AiOutlineSave /> &nbsp; 추천 내역 전체 저장하기
         </Btn>
